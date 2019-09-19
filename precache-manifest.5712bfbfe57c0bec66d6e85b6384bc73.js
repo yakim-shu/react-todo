61262d57b2616fe3909559fbc63eb976d4e3ed66
yakim shu <yakim-shu@users.noreply.github.com>
2019-09-19 13:54:29 +0800

    Todo version-1

---
+++ precache-manifest.5712bfbfe57c0bec66d6e85b6384bc73.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e31c92ce908a45a79c64136efda8081",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "d63fd2af81dd9c1cd8fb",
+    "url": "/react-todo/static/css/main.4609d818.chunk.css"
+  },
+  {
+    "revision": "05d3bf1ae3b802413844",
+    "url": "/react-todo/static/js/2.33763dfb.chunk.js"
+  },
+  {
+    "revision": "d63fd2af81dd9c1cd8fb",
+    "url": "/react-todo/static/js/main.1cb15556.chunk.js"
+  },
+  {
+    "revision": "9c21fcebddc29e391abe",
+    "url": "/react-todo/static/js/runtime~main.23207282.js"
+  },
+  {
+    "revision": "5d1227310dec5d3b4ecffe3b6b798b69",
+    "url": "/react-todo/static/media/check.5d122731.svg"
+  },
+  {
+    "revision": "d4798137877b0297bc4e25c161c7f101",
+    "url": "/react-todo/static/media/delete-button.d4798137.svg"
+  },
+  {
+    "revision": "2927f8de4268bae8f11c4dfe3adc72b5",
+    "url": "/react-todo/static/media/edit.2927f8de.svg"
+  },
+  {
+    "revision": "76d344f08412543c74ccd1fb2493d607",
+    "url": "/react-todo/static/media/github-logo.76d344f0.svg"
+  }
+]);
